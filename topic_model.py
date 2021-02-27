a0d2438b5f01c946190cd590e6a563e838829253
michaelweiss <weiss@sce.carleton.ca>
2021-02-27 13:41:46 -0500

    tokenize documents

--- topic_model.py
+++ topic_model.py
@@ -14,5 +14,18 @@ def show_corpus(corpus):
 	for i, document in enumerate(corpus['content']):
 		print(i, document)
 
+def read_stopwords(file):
+	file = open(file, 'r')
+	return [w.strip() for w in file.read().split('\n')]
+
+def tokenize(document):
+	return [w.lower() for w in document.split()]
+
 corpus = load_corpus("data/assertions.csv")
 show_corpus(corpus)
+
+stopwords = read_stopwords("stopwords-en.txt")
+
+tokens = [[w for w in tokenize(document) if w not in stopwords and w.isalnum()]
+			for document in corpus['content']]
+print(tokens)
