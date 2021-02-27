ab3dd40d7df6943fa68f288a0e88d23aeac10a94
michaelweiss <weiss@sce.carleton.ca>
2021-02-27 14:08:36 -0500

    tokens to bow and fit lda

--- topic_model.py
+++ topic_model.py
@@ -2,9 +2,9 @@
 # simple end-to-end process to create topic co-occurrence graph
 
 import pandas as pd
-from topics import TopicModel
 
-tm = TopicModel()
+from gensim.models import LdaModel
+from gensim.corpora import Dictionary
 
 def load_corpus(file):
 	documents = pd.read_csv(file)
@@ -26,10 +26,22 @@ def corpus_to_tokens(corpus):
 	return [[w for w in tokenize(document) if w not in stopwords and w.isalnum()]
 		for document in corpus['content']]
 
+def tokens_to_bow(tokens, dictionary):
+	return [dictionary.doc2bow(document) for document in tokens]
+
+def fit_lda(tokens, number_of_topics, dictionary):
+	return LdaModel(tokens_to_bow(tokens, dictionary), number_of_topics, dictionary)
+
+def show_topics(lda):
+	for topics in lda.show_topics():
+		print(topics)
+
 #-----------Main----------------
 if __name__ == "__main__":
 	corpus = load_corpus("data/assertions.csv")
 	# show_corpus(corpus)
 	tokens = corpus_to_tokens(corpus)
-	
-	
+	dictionary = Dictionary(tokens)
+	lda = fit_lda(tokens, 10, dictionary)
+	show_topics(lda)
+
