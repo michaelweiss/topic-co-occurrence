948a82e3ee48622c946046cd81ef631ebc47b01d
michaelweiss <weiss@sce.carleton.ca>
2021-02-27 18:59:20 -0500

    create sentences topic co-occurrence matrix

--- topic_model.py
+++ topic_model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# simple end-to-end process to create topic co-occurrence graph
+# simple end-to-end process to create topic co-occurrence matrix
 
 import pandas as pd
 
@@ -40,25 +40,34 @@ def ids_to_words(bow, dictionary):
 	return [(dictionary.id2token[w], f) for w, f in bow]
 
 def document_topics_matrix(bow_list):
-	dtm = []
-	for bow in bow_list:
-		dtm.append(lda.get_document_topics(bow))
-	return dtm
+	return [lda.get_document_topics(bow) for bow in bow_list]
 
 def show_document_topics_matrix(dtm):
 	for i, topics in enumerate(dtm):
 		print(i, topics)
 
+def topic_co_occurrence_matrix(dtm, min_weight=0.1):
+	return [[t for t, w in topics if w >= min_weight] for topics in dtm]
+
+def tcom_to_sentences(tcom):
+	for tco in tcom:
+		tco = ["T{}".format(t) for t in tco]
+		tco.append('.')
+		print(" ".join(tco))
+
 #-----------Main----------------
 if __name__ == "__main__":
-	corpus = load_corpus("data/assertions.csv")
+	corpus = load_corpus("data/abstracts.csv")
 	# show_corpus(corpus)
 	tokens = corpus_to_tokens(corpus)
 	dictionary = Dictionary(tokens)
 	lda = fit_lda(tokens, 10, dictionary)
-	show_topics(lda)
+	# show_topics(lda)
 	dtm = document_topics_matrix(tokens_to_bow(tokens, dictionary))
-	show_document_topics_matrix(dtm)
-
+	# show_document_topics_matrix(dtm)
+	tcom = topic_co_occurrence_matrix(dtm, 0.1)
+	# for i, tco in enumerate(tcom):
+	# 	print(i, tco)
+	tcom_to_sentences(tcom)
 
 
